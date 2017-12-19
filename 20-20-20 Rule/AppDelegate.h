0cfd5105a1a101a9f3f840164ebf5497a2d84690
Rocky <rocky@Rockys-MacBook-Air.local>
2017-12-18 19:04:00 -0600

    initial commit

---
+++ 20-20-20 Rule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20-20-20 Rule
+//
+//  Created by Rocky on 5/6/17.
+//  Copyright © 2017 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20-20-20 Rule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20-20-20 Rule
+//
+//  Created by Rocky on 5/6/17.
+//  Copyright © 2017 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
